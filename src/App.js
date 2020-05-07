1123439f29f83b69f11df9ad92fc28771da5f3dc
Angrey Shoful <shoful.a@gmail.com>
2020-05-07 16:02:41 +0300

    ***

--- src/App.js
+++ src/App.js
@@ -4,137 +4,85 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { cells } from './functions';
 
+import FildGame from './FildGame';
+
 import './App.css';
 
 function App() {
-  const FILD_SIZE = 2;
-  const WIDTH_GAME_FILD = 310;
-  const delay = 2000;
+  const filds = 3;
+  const delay = 1000;
 
-  const [randomCeil, setRandomCeil] = useState([]);
   const [buttonName, setButtonName] = useState('Play');
   const [isDisabled, setIsDisabled] = useState(false);
-  const [rezult, setRezult] = useState(0);
-  // const [option, setOption] = useState(null);
+  const [winner, setWinner] = useState(null);
+  const [isPlay, setIsPlay] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(-1);
-  // const [isStart, setIsStart] = useState(false);
+  const [option, setOption] = useState(null);
 
-  const randomCeilRef = useRef(randomCeil);
-  randomCeilRef.current = randomCeil;
+  const intervalRef = useRef(null);
 
-  const rezultRef = useRef(rezult);
-  rezultRef.current = rezult;
-
-  // useEffect(() => {
-  //   fetch('https://starnavi-frontend-test-task.herokuapp.com/game-settings')
-  //     .then((res) => res.json())
-  //     .then((res) => setOption(res))
-  //     .catch((err) => console.log(err.message));
-  // }, []);
-
-  useEffect(() => {
-    const user = randomCeil.filter((i) => i && i === 'green').length;
-    const computer = randomCeil.filter((i) => i && i === 'red').length;
-    if (user > FILD_SIZE ** 2 / 2) {
-      setRezult(1);
-      console.log('You are winner!!!!');
-    }
-    if (computer > FILD_SIZE ** 2 / 2) {
-      setRezult(2);
-      console.log('Computer is winner!!!!');
-    }
-  }, [randomCeil]);
-
-  const on = (go) => {
-    setTimeout(go, delay);
+  const onInterval = (go) => {
+    intervalRef.current = setInterval(go, delay);
   };
 
-  // const off = () => {
-  //   clearTimeout(timeoutId);
-  // };
+  useEffect(() => {
+    fetch('https://starnavi-frontend-test-task.herokuapp.com/game-settings')
+      .then((res) => res.json())
+      .then((res) => setOption(res))
+      .catch((err) => console.log(err.message));
+  }, []);
 
-  const randomCeilIndex = (arr) => {
+  useEffect(() => {
+    if (winner) {
+      console.log('uEff winner');
+      clearInterval(intervalRef.current);
+      setButtonName('Play again');
+      setIsDisabled(false);
+      setIsPlay(false);
+    }
+  }, [winner]);
+
+  const generateRandomIndex = (arr) => {
     if (!Array.isArray(arr)) {
       return;
     }
     if (currentIndex !== -1) {
       setCurrentIndex(-1);
-      setRandomCeil([]);
+      setIsPlay(false);
+      setWinner(null);
     }
 
     setIsDisabled(true);
+    setIsPlay(true);
     let count = 0;
-    const timer = setInterval(() => {
+    onInterval(() => {
       const index = arr[count];
       setCurrentIndex(index);
-      setRandomCeil((randomCeil) => {
-        on(() => {
-          if (randomCeilRef.current[index] === 'green') {
-            return;
-          }
-          setRandomCeil((randomCeil) => {
-            const newArr = [...randomCeil];
-            newArr[index] = 'red';
-            return [...newArr];
-          });
-        });
-
-        const newArr = [...randomCeil];
-        newArr[index] = 'blue';
-        return [...newArr];
-      });
       count += 1;
-      if (count >= arr.length) {
-        console.log(rezultRef.current);
-        clearInterval(timer);
-        // setCurrentIndex(-1);
-        setButtonName('Play again');
-        setIsDisabled(false);
-      }
-    }, delay);
-  };
-
-  const action = (index) => {
-    if (!(currentIndex === index)) {
-      return;
-    }
-    setRandomCeil((randomCeil) => {
-      const newArr = [...randomCeil];
-      newArr[index] = 'green';
-      return [...newArr];
     });
   };
+  console.log(option);
+  // const { filds: FILD_SIZE, delay } = option.hardMode;
 
-  // console.log(currentIndex);
-  console.log('rezult', rezult);
-  const sizeCell = (WIDTH_GAME_FILD - FILD_SIZE * 2) / FILD_SIZE;
   return (
-    <>
+    <div className="App">
       <button
-        onClick={() => randomCeilIndex(cells(FILD_SIZE ** 2))}
+        className="playButton"
+        onClick={() => generateRandomIndex(cells(filds ** 2))}
         type="button"
         disabled={isDisabled}
       >
         {buttonName}
       </button>
-      <div className="App">
-        <div className="field" style={{ width: WIDTH_GAME_FILD }}>
-          {Array(FILD_SIZE ** 2)
-            .fill('')
-            .map((item, index) => (
-              <div
-                className={
-                  randomCeil[index] ? `ceil ${randomCeil[index]}` : 'ceil'
-                }
-                style={{ width: sizeCell, height: sizeCell }}
-                key={`${index + Date.now()}`}
-                onClick={() => action(index)}
-                role="button"
-              />
-            ))}
-        </div>
-      </div>
-    </>
+      {winner && <p> Победил {winner} </p>}
+      <FildGame
+        currentIndex={currentIndex}
+        isPlay={isPlay}
+        setWinner={setWinner}
+        filds={filds}
+        delay={delay}
+      />
+    </div>
   );
 }
 
